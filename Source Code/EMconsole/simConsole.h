03dd1f9902c9b7941b2587c0138d7c3bc6dc6f6b
David Ge <dge893@gmail.com>
2020-09-25 15:01:29 -0700

    Update simConsole.h

--- Source Code/EMconsole/simConsole.h
+++ Source Code/EMconsole/simConsole.h
@@ -1,8 +1,8 @@
 #pragma once
 /*******************************************************************
-	Author: Bob Limnor (bob@limnor.com, aka Wei Ge)
+	Author:  David Ge (dge893@gmail.com, aka Wei Ge)
 	Last modified: 03/31/2018
-	Allrights reserved by Bob Limnor
+	Allrights reserved by David Ge
 
 ********************************************************************/
 #include "..\EMField\EMField.h"
