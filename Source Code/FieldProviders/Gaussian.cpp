87a7e3b445a0e30852e84bbfd52004ef508dc6a4
David Ge <dge893@gmail.com>
2020-09-25 16:09:56 -0700

    Update Gaussian.cpp

--- Source Code/FieldProviders/Gaussian.cpp
+++ Source Code/FieldProviders/Gaussian.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************
-	Author: Bob Limnor (bob@limnor.com, aka Wei Ge)
+	Author:  David Ge (dge893@gmail.com, aka Wei Ge)
 	Last modified: 03/31/2018
-	Allrights reserved by Bob Limnor
+	Allrights reserved by David Ge
 
 ********************************************************************/
 
